698e2d3f91011fe6929ac4e92b003b5a8a9a6602
JohanssonDaniel <johanssondaniel93@gmail.com>
2017-03-13 15:18:14 +0900

    Fixed bug with i,j

--- Sorting with C-sharp/Sorting with C-sharp/SelectionSort.cs
+++ Sorting with C-sharp/Sorting with C-sharp/SelectionSort.cs
@@ -1,35 +1,27 @@
-﻿namespace Sorting_with_C_sharp
+﻿using System.Collections.Generic;
+namespace Sorting_with_C_sharp
 {
     class SelectionSort
     {
-        public static int[] Sort(int[] list) {
-            //int i, j;
-            int n = list.Length;
-            int[] t = { 1 };
-            //for (i = 0; i < n - 1; i++)
-            //{
-            //    int iMin = i;
-            //    for (j = 0; j < n; j++)
-            //    {
-            //        if (list[j] < list[iMin])
-            //        {
-            //            iMin = j;
-            //        }
-            //    }
-            //    if (iMin != j)
-            //    {
-            //        int temp = list[j];
-            //        list[j] = list[iMin];
-            //        list[iMin] = list[j]; 
-            //    }
-            //}
-            //return list;
-            for (int i = 0; i < n; i++)
+        public static List<int> Sort(List<int> a) {
+            for (int i = 0; i < a.Count - 1; i++)
             {
-                
+                int minVal = i;
+                for (int j = i+1; j < a.Count; j++)
+                {
+                    if (a[j] < a[minVal])
+                    {
+                        minVal = j;
+                    }
+                }
+                if (minVal != i)
+                {
+                    int temp = a[i];
+                    a[i] = a[minVal];
+                    a[minVal] = temp;
+                }
             }
-            System.Console.WriteLine(i);
-            return t;
+            return a;
         }
     }
 }
